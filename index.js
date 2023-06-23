460c7ddfea2d48f9c4ec3308eaf2be7a37ab2133
josphat <mjirura0707@gmail.com>
2023-06-23 17:11:00 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+main.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");
+
+const textNode = document.createTextNode("Josphat is the champion");
+newHeader.appendChild(textNode);
